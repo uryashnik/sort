7f5f66dd6f6cad04c5f9b8081802a80e6739448f
vitaliy <Vitalij.Urjashnik@privatbank.ua>
2021-10-08 19:04:28 +0300

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { Sorter } from './sorter';
 import { NumbersCollection } from './NumbersCollection';
 
-const sorter = new Sorter(new NumbersCollection([4, -3, 11, 2]));
-console.log(sorter.collection);
+const numberCollection = new NumbersCollection([4, -3, 11, 2]);
+console.log(numberCollection);
+const sorter = new Sorter(numberCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numberCollection);
