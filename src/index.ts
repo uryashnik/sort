cdbf16d8e590a9274cc8ebe74cad55d598c87b53
vitaliy <Vitalij.Urjashnik@privatbank.ua>
2021-10-08 08:51:30 +0300

    add sorter skeleton

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,29 @@ class Sorter {
   //   this.collection = collection;
   // }
 
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const { length } = this.collection;
+
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const temp = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = temp;
+        // if collection is array of numbers
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const temp = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = temp;
+          }
+        }
+
+        // if collection is string
+        // some logic for strinngs
+        if (typeof this.collection === 'string') {
+        }
+
+        // if collection is LinkedList
+        if (this.collection instanceof LinkedList) {
         }
       }
     }
