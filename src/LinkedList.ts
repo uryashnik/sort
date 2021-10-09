b6bffd5295fc2163e10d7cc931f98828322bdc0e
vitaliy <Vitalij.Urjashnik@privatbank.ua>
2021-10-09 09:23:11 +0300

    refactoring with abstract class

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -1,10 +1,12 @@
+import { Sorter } from './sorter';
+
 class Node {
   next: Node | null = null;
 
   constructor(public data: number) {}
 }
 
-export class LinkedList {
+export class LinkedList extends Sorter {
   head: Node | null = null;
 
   add(data: number): void {
@@ -36,5 +38,54 @@ export class LinkedList {
     }
 
     return length;
+  }
+
+  at(index: number): Node {
+    if (!this.head) {
+      throw new Error('Index is out of bounds');
+    }
+
+    let counter = 0;
+    let node: Node | null = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+
+      counter++;
+      node = node.next;
+    }
+    throw new Error('Index is out of bounds');
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    if (!this.head) {
+      throw new Error('List is empty');
+    }
+
+    return this.at(leftIndex).data > this.at(rightIndex).data;
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    // упрощенный вариант чтобы не менять обьекты в листе местами
+    // меняем только значения
+    const leftNode = this.at(leftIndex);
+    const rightNode = this.at(rightIndex);
+
+    const lefthand = leftNode.data;
+    leftNode.data = rightNode.data;
+    rightNode.data = lefthand;
+  }
+
+  print(): void {
+    if (!this.head) {
+      return;
+    }
+
+    let node: Node | null = this.head;
+    while (node) {
+      console.log(node.data);
+      node = node.next;
+    }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import { Sorter } from './sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharacterCollection } from './CharacterCollection';
+import { LinkedList } from './LinkedList';
 
 const numberCollection = new NumbersCollection([4, -3, 11, 2]);
 console.log(numberCollection);
-const sorter = new Sorter(numberCollection);
-sorter.sort();
+numberCollection.sort();
 console.log(numberCollection);
 
 const charactersCollection = new CharacterCollection('ZxXYmba');
 console.log(charactersCollection);
-const sorter1 = new Sorter(charactersCollection);
-sorter1.sort();
+charactersCollection.sort();
 console.log(charactersCollection);
+
+const linkedList = new LinkedList();
+linkedList.add(100);
+linkedList.add(-11);
+linkedList.add(-3);
+linkedList.add(7);
+linkedList.print();
+linkedList.sort();
+linkedList.print();
