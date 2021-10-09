3ef75abc676ba0e79ff370bf169975aafba1214b
vitaliy <Vitalij.Urjashnik@privatbank.ua>
2021-10-09 08:19:20 +0300

    add interface Sortable and CharacterCollection

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { Sorter } from './sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharacterCollection } from './CharacterCollection';
 
 const numberCollection = new NumbersCollection([4, -3, 11, 2]);
 console.log(numberCollection);
 const sorter = new Sorter(numberCollection);
 sorter.sort();
 console.log(numberCollection);
+
+const charactersCollection = new CharacterCollection('ZxXYmba');
+console.log(charactersCollection);
+const sorter1 = new Sorter(charactersCollection);
+sorter1.sort();
+console.log(charactersCollection);
